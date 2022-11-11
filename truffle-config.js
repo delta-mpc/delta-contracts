d7f86fed640cffe09cc8fb01e1e1c0e5c3d2f75c
hmiao <739025250@qq.com>
2022-11-11 12:12:20 +0800

    deploy contract by truffle migrate

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,9 @@
+require("dotenv").config()
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+const DELTA_PKEY = process.env.DELTA_PKEY;
+const DELTA_ADDR = process.env.DELTA_ADDR;
+
 module.exports = {
   networks: {
     development: {
@@ -5,6 +11,13 @@ module.exports = {
       port: 8545,
       network_id: "*", // Match any network id
     },
+    delta: {
+      provider: () => {
+        return new HDWalletProvider(DELTA_PKEY, "ws://127.0.0.1:9933")
+      },
+      from: DELTA_ADDR,
+      network_id: "42",
+    }
   },
   compilers: {
     solc: {
